8aa6b66f138d8728c1790e32f675c744aec3dbbd
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-06-08 07:20:52 +0530

    Navbar transparency fixed

--- src/componentes/Navbar/Navbar.js
+++ src/componentes/Navbar/Navbar.js
@@ -1,8 +1,23 @@
 import "./Navbar.css"
+import { useEffect } from "react"
 import React from 'react'
 import { Link } from "react-scroll"
 
 const Navbar = () => {
+
+    const changeColor = () => {
+        var NavBarElement = document.getElementById("Navbar");
+        if(window.scrollY >= 70){
+            NavBarElement.classList.add("transparency-removed");
+        }
+        else{
+            NavBarElement.classList.remove("transparency-removed")
+        }
+    } 
+
+    useEffect(()=>{
+        window.addEventListener("scroll", changeColor)
+    },[])
 return (
     <div className="Nav" id="Navbar">
         <Link to="/">
