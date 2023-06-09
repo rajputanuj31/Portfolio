96a4cade962beafdcdc83fb8ef49ba195d80b0c7
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-06-09 10:34:24 +0530

    Update Navbar.js

--- src/componentes/Navbar/Navbar.js
+++ src/componentes/Navbar/Navbar.js
@@ -1,22 +1,21 @@
 import "./Navbar.css"
-import { useEffect } from "react"
-import React from 'react'
+import React, { useEffect } from "react"
 import { Link } from "react-scroll"
 
 const Navbar = () => {
 
     const changeColor = () => {
-        var NavBarElement = document.getElementById("Navbar");
+        const NavBarElement = document.getElementById("Navbar");
         if(window.scrollY >= 70){
             NavBarElement.classList.add("transparency-removed");
         }
         else{
-            NavBarElement.classList.remove("transparency-removed")
+            NavBarElement.classList.remove("transparency-removed");
         }
     } 
 
     useEffect(()=>{
-        window.addEventListener("scroll", changeColor)
+        window.addEventListener("scroll", changeColor);
     },[])
 return (
     <div className="Nav" id="Navbar">
