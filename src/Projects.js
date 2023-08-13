cf4ce42c509117f67d61bd61e41e65387d81082a
Anuj Rajput <rajputanuj31>
2023-08-14 00:45:35 +0530

    Change projects view

---
+++ src/Projects.js
@@ -0,0 +1,44 @@
+import Lumo from "./asset/Lumos.png"
+import Todo from "./asset/Todo.jpg"
+import Wine from "./asset/Wine.png"
+import joomla from "./asset/Joomla-Project.png"
+const details = [
+  {
+    "name": "Open Source Project",
+    "subheading": "Automated testing Project using Cypress in Joomla-cms",
+    "description": "",
+    "techstack": ["Cypress ","20+ PR's merged"],
+    "github": "https://github.com/joomla/joomla-cms/pulls?q=is%3Apr+author%3Arajputanuj31+is%3Aclosed",
+    "Demo": "",
+    "image": joomla
+  },
+  {
+    "name": "Lumos Labs Clone",
+    "subheading": "Fully Functioning, Responsive",
+    "description": "",
+    "techstack": ["Html ", "Css ", "ReactJS "],
+    "github": "https://github.com/rajputanuj31/Lumos-Labs-Clone",
+    "Demo": "https://main--nimble-cajeta-31a182.netlify.app/",
+    "image": Lumo
+  },
+  {
+    "name": "To do App",
+    "subheading": "Fully Functioning, Responsive, Use Local Storage to store tasks",
+    "description": "",
+    "techstack": ["Html ", "Css ", "ReactJS "],
+    "github": "https://github.com/rajputanuj31/Daily_Goals",
+    "Demo": "https://rajputanuj31.github.io/Daily_Goals/",
+    "image": Todo
+  },
+  {
+    "name": "Wine Shop",
+    "subheading": "Simple front page of wine shop",
+    "description": "",
+    "techstack": ["Html ", "Css "],
+    "github": "https://github.com/rajputanuj31/madirashop",
+    "Demo": "https://rajputanuj31.github.io/madirashop/",
+    "image": Wine
+  }
+]
+
+export default details;
--- src/componentes/Projects/Projects.js
+++ src/componentes/Projects/Projects.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import "./Projects.css"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import project1 from "../../asset/project1.jpg"
-import project2 from "../../asset/project2.jpg"
-import project3 from "../../asset/project3.jpg"
-import project4 from "../../asset/Lumos.png"
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import 'swiper/css'
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import details from '../../Projects.js'
 
 const Projects = () => {
   return (
@@ -17,99 +8,24 @@ const Projects = () => {
       <div className='recent-project'>
         <span>Recent Projects</span>
       </div>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={10}
-        slidesPerView={1}
-        grabCursor={true}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        className='projects-slider'
-      >
-        <SwiperSlide>
-          <div className='tech'>
-            <span >Lumos Labs Clone</span>
+      {details.map((item,index)=>{
+        return (
+          <div className='pr'>
+            <div className='pr-left' key={index}>
+              <ul>
+                <h1 style={{color:"white",paddingBottom:"1rem"}}>{item.name}</h1>
+                <li>{item.subheading}</li>
+                <li>{item.techstack}</li>
+                <li><a href={item.github} style={{color:"white",textDecoration:"Underline"}}>Github</a></li>
+                <li><a href={item.Demo} style={{color:"white",textDecoration:"Underline"}}>Demo</a></li>
+              </ul>
+              </div>
+              <div className='pr-right' key={index}>
+              <img src={item.image} alt="" />
+              </div>
           </div>
-          <img src={project4} alt="" />
-          <div className='details'>
-            <div class="text">
-              <li>Fully Functioning</li>
-              <li>Html, Css, React</li>
-              <li>Responsive</li>
-            </div>
-          </div>
-          <div className='buttons'>
-            <a href="https://github.com/rajputanuj31/Lumos-Labs-Clone" target=" ">
-              <button className='btn btn-light' style={{ marginLeft: "6rem" }} >Github</button>
-            </a>
-            <a href="https://main--nimble-cajeta-31a182.netlify.app/" target=" ">
-              <button className='btn btn-light'>Demo</button>
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='tech'>
-            <span >To do App</span>
-          </div>
-          <img src={project2} alt="" />
-          <div className='details'>
-            <div class="text">
-              <li>Fully Functioning</li>
-              <li>Html, Css, React</li>
-              <li>Responsive</li>
-              <li>Use Local Storage</li>
-            </div>
-          </div>
-          <div className='buttons'>
-            <a href="https://github.com/rajputanuj31/Daily_Goals" target=" ">
-              <button className='btn btn-light' style={{ marginLeft: "6rem" }} >Github</button>
-            </a>
-            <a href="https://rajputanuj31.github.io/Daily_Goals/" target=" ">
-              <button className='btn btn-light'>Demo</button>
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='tech'>
-            <span >Wine Shop</span>
-          </div>
-          <img src={project1} alt="" />
-          <div className='details'>
-            <div class="text">
-              <li>Simle Frontend</li>
-              <li>Html, Css</li>
-              <li>Responsive</li>
-            </div>
-          </div>
-          <div className='buttons'>
-            <a href="https://github.com/rajputanuj31/madirashop" target=" ">
-              <button className='btn btn-light' style={{ marginLeft: "6rem" }} >Github</button>
-            </a>
-            <a href="https://rajputanuj31.github.io/madirashop/" target=" ">
-              <button className='btn btn-light'>Demo</button>
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='tech'>
-            <span >Music App</span>
-          </div>
-          <img src={project3} alt="" />
-          <div className='details'>
-            <div class="text">
-              <li>Fully Functioning</li>
-              <li>Html, Css, Javascript</li>
-              <li>Responsive</li>
-              <li>Song search function</li>
-            </div>
-          </div>
-          <div className='buttons'>
-            <button className='btn btn-light' style={{ marginLeft: "6rem" }} >Github</button>
-            <button className='btn btn-light'>Demo</button>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+        )
+      }) }
     </div>
   )
 }
